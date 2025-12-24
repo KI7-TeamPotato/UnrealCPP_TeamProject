8cd53e3b0dfaa80e6747370e78e08cde336c05d5
LDY <137361731+ldsldy@users.noreply.github.com>
2025-12-24 16:41:25 +0900

    캐릭터 선택창의 캐릭터 디테일 패널 제작 시작

---
+++ Source/TeamPotato/Public/UI/CharacterSelect/CharacterSelectDetailWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacterSelectDetailWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMPOTATO_API UCharacterSelectDetailWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
