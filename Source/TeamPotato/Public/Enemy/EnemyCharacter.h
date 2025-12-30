d85939ace7801b30704246e3152c82bdbb87d9e5
Leedohyeon63 <dhanrfk27@gmail.com>
2025-12-30 19:26:13 +0900

    보스버그수정

--- Source/TeamPotato/Public/Enemy/EnemyCharacter.h
+++ Source/TeamPotato/Public/Enemy/EnemyCharacter.h
@@ -100,11 +100,9 @@ protected:
     FTimerHandle InvincibilityTimerHandle;
 
     UPROPERTY(EditAnywhere, Category = "Combat")
-<<<<<<< Updated upstream
+
     float InvincibilityDuration = 0.1f;
-=======
-    float InvincibilityDuration = 0.5f;
->>>>>>> Stashed changes
+
 
     void ResetInvincibility();
 private:
