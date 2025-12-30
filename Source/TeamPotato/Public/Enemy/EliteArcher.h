c6348f33451a6e636acf0c66a985fa9dd161ea64
Leedohyeon63 <dhanrfk27@gmail.com>
2025-12-31 03:22:20 +0900

    엘리트 두마리 추가

---
+++ Source/TeamPotato/Public/Enemy/EliteArcher.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/ArcherEnemy.h"
+#include "EliteArcher.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMPOTATO_API AEliteArcher : public AArcherEnemy
+{
+	GENERATED_BODY()
+    AEliteArcher();
+
+    //인터페이스 함수 오버라이드, EnumBase에 있는 열거형에 따라 상태별 속도 설정
+    virtual float SetMovementSpeed_Implementation(EEnemySpeed State) override;
+};
